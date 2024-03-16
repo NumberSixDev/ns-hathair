d3be38c9bd586ca273fcb6403c07bbee9efebded
NumberSixDev <38400768+NumberSixDev@users.noreply.github.com>
2024-03-16 03:11:31 +0000

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,12 @@
+	["hairband"] = {
+		["name"] = "hairband",
+		["label"] = "Hair Band",
+		["weight"] = 0,
+		["type"] = "item",
+		["image"] = "hairband.png",
+		["unique"] = false,
+		["useable"] = true,
+		["shouldClose"] = true,
+		["combinable"] = nil,
+		["description"] = "Make Your Hair Disappear"
+	},
